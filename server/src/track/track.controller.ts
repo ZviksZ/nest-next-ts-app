bf6c0739981ceac40a6f4fb3765c3a1695d6fed0
Nikita Kapshtyk <kapshtyk.n@globalsolutions.ru>
2021-02-26 17:54:35 +0300

    learning basic nest js, starting structure

---
+++ server/src/track/track.controller.ts
@@ -0,0 +1,20 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('/tracks')
+export class TrackController {
+  create() {
+
+  }
+
+  getAll() {
+
+  }
+
+  getOne() {
+
+  }
+
+  delete() {
+
+  }
+}
